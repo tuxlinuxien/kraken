233ef5a4113020aa8e33ead4e90a148593b3d574
Yoann Cerda <tuxlinuxien@gmail.com>
2021-12-22 19:13:44 +0800

    add balance ex

--- src/http/private.rs
+++ src/http/private.rs
@@ -5,3 +5,7 @@ use super::client::Method;
 pub async fn balance<T: BaseClient>(clt: &T) -> Result<String, Error> {
     return clt.private(Method::POST, "/0/private/Balance", &[]).await;
 }
+
+pub async fn balance_ex<T: BaseClient>(clt: &T) -> Result<String, Error> {
+    return clt.private(Method::POST, "/0/private/BalanceEx", &[]).await;
+}
