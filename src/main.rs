32619b74cca7cccf3d93132be0518385990fdf80
Yoann Cerda <tuxlinuxien@gmail.com>
2021-12-28 11:36:27 +0800

    add public methods and json output.

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,16 @@
 use anyhow::{anyhow, Context, Result};
 use clap::{App, Arg, SubCommand};
 use kraken::api;
-use std::{fmt::Debug, ops::Deref};
+use serde::Serialize;
+use serde_json::to_string_pretty;
 
 fn display<T>(output: T)
 where
-    T: Debug,
+    T: Serialize,
 {
-    println!("{:?}", output);
+    if let Ok(out) = to_string_pretty(&output) {
+        println!("{}", out);
+    }
 }
 
 #[tokio::main]
@@ -85,6 +88,26 @@ async fn main() -> Result<(), anyhow::Error> {
                 )
                 .arg(Arg::with_name("count").long("count").takes_value(true)),
         )
+        .subcommand(
+            SubCommand::with_name("trades")
+                .arg(
+                    Arg::with_name("pair")
+                        .long("pair")
+                        .takes_value(true)
+                        .required(true),
+                )
+                .arg(Arg::with_name("count").long("count").takes_value(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("spread")
+                .arg(
+                    Arg::with_name("pair")
+                        .long("pair")
+                        .takes_value(true)
+                        .required(true),
+                )
+                .arg(Arg::with_name("count").long("count").takes_value(true)),
+        )
         .get_matches();
     match matches.subcommand_name() {
         Some("time") => display(api::public::time().await?),
@@ -104,10 +127,7 @@ async fn main() -> Result<(), anyhow::Error> {
             let cmd = matches.subcommand_matches("asset-pair").unwrap();
             display(
                 api::public::asset_pair(
-                    cmd.values_of("pair")
-                        .unwrap()
-                        .collect::<Vec<&str>>()
-                        .deref(),
+                    &cmd.values_of("pair").unwrap().collect::<Vec<&str>>(),
                     cmd.value_of("info"),
                 )
                 .await?,
@@ -133,7 +153,7 @@ async fn main() -> Result<(), anyhow::Error> {
             } else {
                 None
             };
-            display(api::public::ohcl(pair, interval, since).await)
+            display(api::public::ohcl(pair, interval, since).await?)
         }
         Some("depth") => {
             let cmd = matches.subcommand_matches("depth").unwrap();
@@ -150,6 +170,38 @@ async fn main() -> Result<(), anyhow::Error> {
                 None
             };
             display(api::public::depth(pair, count).await?)
+        }
+        Some("trades") => {
+            let cmd = matches.subcommand_matches("trades").unwrap();
+            let pair = cmd.value_of("pair").unwrap();
+            let count: Option<i64> = if let Some(val) = cmd.value_of("count") {
+                let val = val
+                    .parse::<i64>()
+                    .context("count is not an valid integer")?;
+                if val < 0 || val > 500 {
+                    return Err(anyhow!("count must be between 0 to 500"));
+                }
+                Some(val)
+            } else {
+                None
+            };
+            display(api::public::trades(pair, count).await?)
+        }
+        Some("spread") => {
+            let cmd = matches.subcommand_matches("spread").unwrap();
+            let pair = cmd.value_of("pair").unwrap();
+            let count: Option<i64> = if let Some(val) = cmd.value_of("count") {
+                let val = val
+                    .parse::<i64>()
+                    .context("count is not an valid integer")?;
+                if val < 0 || val > 500 {
+                    return Err(anyhow!("count must be between 0 to 500"));
+                }
+                Some(val)
+            } else {
+                None
+            };
+            display(api::public::spread(pair, count).await?)
         }
         Some(&_) => {}
         None => {}
